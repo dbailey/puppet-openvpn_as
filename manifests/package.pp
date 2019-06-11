8fe67ff816e14d5798ea251d2c310ff983283cf8
Dakota Bailey <dakota@sheerid.com>
2019-06-11 01:30:46 -0600

    Require ncurses-compat-libs for AWS Linux 2

--- manifests/package.pp
+++ manifests/package.pp
@@ -4,7 +4,9 @@ class openvpn_as::package {
 
   # The OpenVPN-AS package:
   package { 'openvpn-as':
-    ensure  => $openvpn_as::package_version
+    ensure  => $openvpn_as::package_version,
+    require => Package['ncurses-compat-libs']
   }
 
+  pacakge { 'ncurses-compat-libs': }
 }
